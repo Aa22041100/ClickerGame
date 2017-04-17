feeab85043b6e88f1ba925c74b25b7be6bf6e076
Stanley Lai <stanley.lai.104@gmail.com>
2017-04-17 10:33:50 +0800

    Added updating method for UISkill

--- Assets/Scripts/UI/Controller/UISkillController.cs
+++ Assets/Scripts/UI/Controller/UISkillController.cs
@@ -6,6 +6,9 @@ public class UISkillController : MonoBehaviour {
 
 	public UISkill skillTemplate;
 
+	// cache reference list
+	public List<UISkill> cache;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (InitSkillPanel ());
@@ -16,13 +19,51 @@ public class UISkillController : MonoBehaviour {
 		
 	}
 
+	/// <summary>
+	/// Initialize the UI skill panel.
+	/// </summary>
+	/// <returns>Async method to clone template to the UISkillPanel parent.</returns>
 	IEnumerator InitSkillPanel() {
 		List<Skill> tmp = SkillLoader.LoadSkill (Constants.SKILL_DATA_PATH);
 		for (int i = 0; i < tmp.Count; i++) {
 			GameObject go = Instantiate (skillTemplate.gameObject, this.transform);
-			go.GetComponent<UISkill> ().SetupSkill (tmp [i].GetSkillName (), tmp [i].GetSkillDesc ());
+			go.GetComponent<UISkill> ().SetupSkill (tmp [i].GetSkillName (), tmp [i].GetSkillDesc (), SkillManager.GetSkillLevel(i).ToString(), i);
 			go.SetActive (true);
+
+			// added to cache
+			cache.Add (go.GetComponent<UISkill> ());
 		}
 		yield return null;
+	}
+
+	/// <summary>
+	/// Update the UISkillPanel elements
+	/// </summary>
+	/// <param name="id">Skill ID</param>
+	public void UpdateSkillInfo(int id) {
+		if (cache.Count < id) {
+			// avoid array out of range
+			return;
+		} else {
+			cache [id].UpdateSkillLevel (SkillManager.GetSkillLevel (id).ToString());
+		}
+	}
+
+	/// <summary>
+	/// Updates all the UISkillPanel level elements
+	/// </summary>
+	public void UpdateSkillInfo() {
+		for (int i = 0; i < cache.Count; i++) {
+			cache [i].UpdateSkillLevel (SkillManager.GetSkillLevel (i).ToString());
+		}
+	}
+
+	/// <summary>
+	/// Reciever for level up message
+	/// </summary>
+	/// <param name="id">Skill Id</param>
+	public void _SkillLevelUp (int id) {
+		SkillManager.SkillLevelUp (id);
+		UpdateSkillInfo (id);
 	}
 }
--- Assets/Scripts/UI/Views/UISkill.cs
+++ Assets/Scripts/UI/Views/UISkill.cs
@@ -8,8 +8,14 @@ public class UISkill : MonoBehaviour {
 	// view columns
 	public Text name;
 	public Text desc;
+	public Text level;
 	public Button lvupBtn;
 	public Image icon;
+
+	// identify usage
+	protected int id;
+
+	const string skillFormatStr = "Lv: {0}";
 
 	// Use this for initialization
 	void Start () {
@@ -21,8 +27,28 @@ public class UISkill : MonoBehaviour {
 		
 	}
 
-	public void SetupSkill(string name, string desc) {
+	/// <summary>
+	/// Setup UISkill for the first load.
+	/// </summary>
+	/// <param name="name">Skill Name</param>
+	/// <param name="desc">Skill Description</param>
+	/// <param name="level">Current Skill Level</param>
+	public void SetupSkill(string name, string desc, string level, int id) {
 		this.name.text = name;
 		this.desc.text = desc;
+		this.level.text = string.Format(skillFormatStr, level);
+		this.id = id;
+	}
+
+	/// <summary>
+	/// Updates the skill level.
+	/// </summary>
+	/// <param name="level">Current Skill Level</param>
+	public void UpdateSkillLevel(string level) {
+		this.level.text = string.Format(skillFormatStr, level);
+	}
+
+	public void SendLevelUpMsg() {
+		GameObject.FindObjectOfType<UISkillController> ().SendMessage ("_SkillLevelUp", id);
 	}
 }
