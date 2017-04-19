0045bb58230f6eaab3570360dcedfd5149dd790d
Stanley Lai <stanley.lai.104@gmail.com>
2017-04-19 09:58:06 +0800

    added switch tab for bottom control panel

---
+++ Assets/Scripts/UI/Controller/UIBottomPanelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBottomPanelController : MonoBehaviour {
+
+	public Transform skillPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetSkillPanelOnTop() {
+		skillPanel.SetAsLastSibling ();
+	}
+}
